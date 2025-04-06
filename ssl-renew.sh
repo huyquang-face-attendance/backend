6b98adeb2c360ecf270cb0934eeeba1bd1e3c803
huyquang-bka <huyquangbka@gmail.com>
2025-04-06 15:35:58 +0700

    ...

--- ssl-renew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-cd /path/to/your/project
-$COMPOSE run certbot renew --force-renewal
-$COMPOSE restart nginx 
